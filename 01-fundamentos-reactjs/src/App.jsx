b681f01d627a454d488251bcb5af2a5e1433b95f
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-01-30 23:55:16 -0300

    sobre propriedades e componentes

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,16 @@
-function App() {
+import { Post } from './Post'
+
+export function App () {
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <Post
+        author='Lucas Araujo'
+        content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet veniam sint laborum mollitia voluptas eum, voluptate rerum id dolor cupiditate deserunt, iusto possimus ipsam! Quisquam consequatur odio a tempore sunt.'
+      />
+      <Post
+        author='Antonio Araujo'
+        content='Um novo post muito legal'
+      />
+    </div>
   )
 }
-
-export default App
